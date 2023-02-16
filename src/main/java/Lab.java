e054350a6e12f5b3c93830f907815df7d5821207
vtran91 <vtran16@bellsouth.net>
2023-02-16 15:54:02 +0000

    Completed set lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -20,7 +21,8 @@ public class Lab {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        Set<Integer> set = new HashSet<Integer>();
+        return set;
     }
 
     /**
@@ -29,7 +31,7 @@ public class Lab {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        return set.size();
     }
 
     /**
@@ -39,7 +41,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
-
+        set.add(item);
     }
 
     /**
@@ -49,6 +51,6 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
-
+        set.remove(item);
     }
 }
